9ff62a75c353f3173282910a1d8b27fd591da958
huoqi <huoqi@yonyou.com>
2015-05-30 11:19:35 +0800

    监听应用启动

---
+++ src/main/java/uap/ae/kepler/controller/DemoServletContextListener.java
@@ -0,0 +1,16 @@
+package uap.ae.kepler.controller;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+public class DemoServletContextListener implements ServletContextListener {
+
+	@Override
+	public void contextDestroyed(ServletContextEvent event) {
+	}
+
+	@Override
+	public void contextInitialized(ServletContextEvent event) {
+	}
+
+}
